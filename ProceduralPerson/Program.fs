6901b5333e88eea60275b0fab0e80836045b0671
Daniel Markham <DanielBMarkham@Hotmail.com>
2020-09-29 10:21:39 -0400

    Initial load

---
+++ ProceduralPerson/Program.fs
@@ -0,0 +1,14 @@
+﻿open RandomPerson
+open System
+
+[<EntryPoint>]
+let main argv =
+  printfn "Hello "
+  printfn "      %s %s" (pickRandomFirstName()) (pickRandomLastName())
+  printfn "      %s %s" (pickRandomFirstName()) (pickRandomLastName())
+  printfn "      %s %s" (pickRandomFirstName()) (pickRandomLastName())
+  printfn "      %s %s" (pickRandomFirstName()) (pickRandomLastName())
+  printfn "      %s %s" (pickRandomFirstName()) (pickRandomLastName())
+  printfn "      %s %s" (pickRandomFirstName()) (pickRandomLastName())
+  printfn "      %s %s" (pickRandomFirstName()) (pickRandomLastName())
+  0
---
+++ ProceduralPerson/RandomPerson.fs
@@ -0,0 +1,23 @@
+﻿module RandomPerson
+open LastNames
+open FirstNames
+open System
+
+let inline upperCaseFirstLetter (s:string) =
+  let mutable c=s.ToLower().ToCharArray()
+  c.[0]<-Char.ToUpper(c.[0])
+  new string(c)
+
+let pickRandomLastName() =
+  let rnd=Random()
+  let newNum=rnd.NextDouble()*100.0
+  let filteredList=lastNameAndFrequencies|>List.filter(fun x->newNum<(snd x))
+  let chosen=filteredList.[0]
+  upperCaseFirstLetter (fst chosen)
+
+let pickRandomFirstName() =
+  let rnd=Random()
+  let newNum=rnd.NextDouble()*100.0
+  let filteredList=firstNameAndFrequencies|>List.filter(fun x->newNum<(snd x))
+  let chosen=filteredList.[0]
+  upperCaseFirstLetter (fst chosen)
